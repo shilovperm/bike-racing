8cbf51a9f94aadc2ef7024d9d01116aef0b595b3
shilovperm <46396241+shilovperm@users.noreply.github.com>
2019-01-05 18:23:09 +0500

    page-events

---
+++ page-events.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Страница с кастомным шаблоном (page-events.php)
+ * @package WordPress
+ * @subpackage bike-racing
+ * Template Name: page-events
+ */
+get_header(); // подключаем header.php ?>
+<section>
+	<div class="container">
+		<div class="row">
+			<div class="<?php content_class_by_sidebar(); // функция подставит класс в зависимости от того есть ли сайдбар, лежит в functions.php ?>">
+				<?php if ( have_posts() ) while ( have_posts() ) : the_post(); // старт цикла ?>
+					<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>> <?php // контэйнер с классами и id ?>
+						<h1><?php the_title(); // заголовок поста ?></h1>
+						<?php the_content(); // контент ?>
+					</article>
+				<?php endwhile; // конец цикла ?>
+			</div>
+			<?php get_sidebar(); // подключаем sidebar.php ?>
+		</div>
+	</div>
+</section>
+<?php get_footer(); // подключаем footer.php ?>
