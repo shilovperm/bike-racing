f9b1986578247c647d4abdae46fd9dc0296b73d4
shilovperm <46396241+shilovperm@users.noreply.github.com>
2019-01-08 19:27:10 +0500

    version 0.1 08.01.2019

--- index.php
+++ index.php
@@ -12,96 +12,157 @@ get_header(); // подключаем header.php ?>
         <section class="next-events bordered">
           <div class="block-label">next-events</div>
           <h2>Ближайшие события</h2>
-          <div class="card-deck card-deck-custom">
-            <div class="card card-custom">
-              <img src="<?php echo get_template_directory_uri() . '/images/veltasport.png'; ?>" class="card-img-top card-img-top-custom" alt="Велта-спорт">
-              <div class="card-body">
-                <h5 class="card-title">Кубок Велта-спорт</h5>
-                <p class="card-text">I этап "Открытие сезона" MTB</p>
-                <p class="card-text"><small class="text-muted">4 марта 2018</small></p>
+          <div class="container-events">
+
+            <!-- Card Wider -->
+            <div class="card card-cascade wider card-custom position-relative blue-grey-text">
+              <!-- sun image -->
+              <i class="fas fa-sun position-absolute absolute-top-left text-danger "></i>
+              <!-- bicycle image -->
+              <i class="fas fa-bicycle position-absolute absolute-top-right text-danger"></i>
+              <!-- Card image -->
+              <div class="view view-cascade overlay">
+                <img  class="card-img-top card-img-top-custom yellow-background" src="<?php echo get_template_directory_uri() . '/images/veltasport.png'; ?>" alt="Card image cap">
+                <a href="#!">
+                  <div class="mask rgba-white-slight"></div>
+                </a>
+              </div>
+              <!-- Card content -->
+              <div class="card-body card-body-cascade text-center">
+                <!-- Title -->
+                <h4 class="card-title"><strong>Кубок Велта-спорт</strong></h4>
+                <!-- Subtitle -->
+                <h5 class="blue-text pb-2"><strong>Этап №2 "Бодрый заряд"</strong></h5>
+                <!-- Date -->
+                <p class="mb-0"><i class="fas fa-calendar mr-2"></i>13.05.2018</p>
+                <!-- Text -->
+                <p class="card-text">Профсоюзка - это народный бренд</p>
+                <!-- Button -->
+                <button type="button" class="btn btn-outline-success waves-effect">Зарегистрироваться</button>
               </div>
             </div>
-            <div class="card card-custom">
-              <img src="<?php echo get_template_directory_uri() . '/images/veltasport.png'; ?>" class="card-img-top card-img-top-custom" alt="Велта-спорт">
-              <div class="card-body">
-                <h5 class="card-title">Кубок Велта-спорт</h5>
-                <p class="card-text">II этап "Бодрый заряд" MTB</p>
-                <p class="card-text"><small class="text-muted">13 мая 2018</small></p>
+            <!-- Card Wider -->
+            <!-- Card Wider -->
+            <div class="card card-cascade wider card-custom position-relative blue-grey-text">
+              <!-- cloud-sun image -->
+              <i class="fas fa-cloud-sun position-absolute absolute-top-left text-primary"></i>
+              <!-- bicycle image -->
+              <i class="fas fa-bicycle position-absolute absolute-top-right text-danger"></i>
+              <!-- Card image -->
+              <div class="view view-cascade overlay">
+                <img  class="card-img-top card-img-top-custom yellow-background" src="<?php echo get_template_directory_uri() . '/images/veltasport.png'; ?>" alt="Card image cap">
+                <a href="#!">
+                  <div class="mask rgba-white-slight"></div>
+                </a>
+              </div>
+              <!-- Card content -->
+              <div class="card-body card-body-cascade text-center">
+                <!-- Title -->
+                <h4 class="card-title"><strong>Кубок Велта-спорт</strong></h4>
+                <!-- Subtitle -->
+                <h5 class="blue-text pb-2"><strong>Этап №3 "РОВный пульс"</strong></h5>
+                <!-- Date -->
+                <p class="mb-0"><i class="fas fa-calendar mr-2"></i>17.06.2018</p>
+                <!-- Text -->
+                <p class="card-text">Рваный пульс на РОВной трассе</p>
+                <!-- Button -->
+                <button type="button" class="btn btn-outline-gray" disabled>Ожидание регистрации</button>
               </div>
             </div>
-            <div class="card card-custom">
-              <img src="<?php echo get_template_directory_uri() . '/images/veltasport.png'; ?>" class="card-img-top card-img-top-custom" alt="Велта-спорт">
-              <div class="card-body">
-                <h5 class="card-title">Кубок Велта-спорт</h5>
-                <p class="card-text">III этап "Ровный пульс" MTB</p>
-                <p class="card-text"><small class="text-muted">17 июня 2018</small></p>
+            <!-- Card Wider -->
+            <!-- Card Wider -->
+            <div class="card card-cascade wider card-custom position-relative blue-grey-text">
+              <!-- umbrella image -->
+              <i class="fas fa-umbrella position-absolute absolute-top-left text-primary"></i>
+              <!-- bicycle image -->
+              <i class="fas fa-bicycle position-absolute absolute-top-right text-danger"></i>
+              <!-- Card image -->
+              <div class="view view-cascade overlay">
+                <img  class="card-img-top card-img-top-custom yellow-background" src="<?php echo get_template_directory_uri() . '/images/veltasport.png'; ?>" alt="Card image cap">
+                <a href="#!">
+                  <div class="mask rgba-white-slight"></div>
+                </a>
+              </div>
+              <!-- Card content -->
+              <div class="card-body card-body-cascade text-center">
+                <!-- Title -->
+                <h4 class="card-title"><strong>Кубок Велта-спорт</strong></h4>
+                <!-- Subtitle -->
+                <h5 class="blue-text pb-2"><strong>Этап №4 "ДИВное лето"</strong></h5>
+                <!-- Date -->
+                <p class="mb-0"><i class="fas fa-calendar mr-2"></i>29.07.2018</p>
+                <!-- Text -->
+                <p class="card-text">Лужа в твоих бошмаках зазвучит соль диезом</p>
+                <!-- Button -->
+                <button type="button" class="btn btn-outline-gray" disabled>Ожидание регистрации</button>
               </div>
             </div>
-            <div class="card card-custom">
-              <img src="<?php echo get_template_directory_uri() . '/images/veltasport.png'; ?>" class="card-img-top card-img-top-custom" alt="Велта-спорт">
-              <div class="card-body">
-                <h5 class="card-title">Кубок Велта-спорт</h5>
-                <p class="card-text">IV этап "ДИВное лето" MTB</p>
-                <p class="card-text"><small class="text-muted">29 июля 2018</small></p>
+            <!-- Card Wider -->
+            <!-- Card Wider -->
+            <div class="card card-cascade wider card-custom position-relative blue-grey-text">
+              <!-- piggy-bank image -->              
+              <i class="fas fa-piggy-bank position-absolute absolute-top-left text-black-50"></i>
+              <!-- bicycle image -->
+              <i class="fas fa-bicycle position-absolute absolute-top-right text-danger"></i>
+              <!-- Card image -->
+              <div class="view view-cascade overlay">
+                <img  class="card-img-top card-img-top-custom yellow-background" src="<?php echo get_template_directory_uri() . '/images/veltasport.png'; ?>" alt="Card image cap">
+                <a href="#!">
+                  <div class="mask rgba-white-slight"></div>
+                </a>
+              </div>
+              <!-- Card content -->
+              <div class="card-body card-body-cascade text-center">
+                <!-- Title -->
+                <h4 class="card-title"><strong>Кубок Велта-спорт</strong></h4>
+                <!-- Subtitle -->
+                <h5 class="blue-text pb-2"><strong>Этап №5 "Andronovo Mud Race"</strong></h5>
+                <!-- Date -->
+                <p class="mb-0"><i class="fas fa-calendar mr-2"></i>27.10.2018</p>
+                <!-- Text -->
+                <p class="card-text">Холодная грязь. Самовывоз. Дорого.</p>
+                <!-- Button -->
+                <button type="button" class="btn btn-outline-gray" disabled>Ожидание регистрации</button>
               </div>
             </div>
-            <div class="card card-custom">
-              <img src="<?php echo get_template_directory_uri() . '/images/veltasport.png'; ?>" class="card-img-top card-img-top-custom" alt="Велта-спорт">
-              <div class="card-body">
-                <h5 class="card-title">Кубок Велта-спорт</h5>
-                <p class="card-text">V этап "Andronovo Mud Race" MTB</p>
-                <p class="card-text"><small class="text-muted">27 октября 2018</small></p>
-              </div>
-            </div>
+            <!-- Card Wider -->
           </div>
         </section>
         <section class="past-events bordered">
           <div class="block-label">past-events</div>
           <h2>Прошедшие события</h2>
           <div class="container-events">
-            <article class="event-block velta-background">
-              <h4 class="event-header">Кубок Велта-спорт</h4>
-              <p class="event-stage">I этап</p>
-              <p class="event-date">04.03.2018</p>
-            </article>
-            <article class="event-block velta-background">
-              <h4 class="event-header">Кубок Велта-спорт</h4>
-              <p class="event-stage">II этап</p>
-              <p class="event-date">04.03.2018</p>
-            </article>
-            <article class="event-block velta-background">
-              <h4 class="event-header">Кубок Велта-спорт</h4>
-              <p class="event-stage">III этап</p>
-              <p class="event-date">04.03.2018</p>
-            </article>
-            <article class="event-block velta-background">
-              <h4 class="event-header">Кубок Велта-спорт</h4>
-              <p class="event-stage">IV этап</p>
-              <p class="event-date">04.03.2018</p>
-            </article>
-            <article class="event-block velta-background">
-              <h4 class="event-header">Кубок Велта-спорт</h4>
-              <p class="event-stage">V этап</p>
-              <p class="event-date">04.03.2018</p>
-            </article>
+            <!-- Card Wider -->
+            <div class="card card-cascade wider card-custom position-relative blue-grey-text">
+              <!-- snowflake image -->
+              <i class="fas fa-snowflake position-absolute absolute-top-left text-info"></i>
+              <!-- bicycle image -->
+              <i class="fas fa-bicycle position-absolute absolute-top-right text-danger"></i>
+              <!-- Card image -->
+              <div class="view view-cascade overlay">
+                <img  class="card-img-top card-img-top-custom yellow-background" src="<?php echo get_template_directory_uri() . '/images/veltasport.png'; ?>" alt="Card image cap">
+                <a href="#!">
+                  <div class="mask rgba-white-slight"></div>
+                </a>
+              </div>
+              <!-- Card content -->
+              <div class="card-body card-body-cascade text-center">
+                <!-- Title -->
+                <h4 class="card-title"><strong>Кубок Велта-спорт</strong></h4>
+                <!-- Subtitle -->
+                <h5 class="blue-text pb-2"><strong>Этап №1 "Открытие сезона"</strong></h5>
+                <!-- Date -->
+                <p class="mb-0"><i class="fas fa-calendar mr-2"></i>04.03.2018</p>
+                <!-- Text -->
+                <p class="card-text">Первая зимняя гонка от Сварочника и Димы Че</p>
+                <!-- Button -->
+                <button type="button" class="btn btn-success" disabled>Гонка завершена</button>
+              </div>
+            </div>
+            <!-- Card Wider -->
           </div>
         </section>
-        <!--
-        <div class="<?php content_class_by_sidebar(); // функция подставит класс в зависимости от того есть ли сайдбар, лежит в functions.php ?>">
-  				<h1>Hello world<?php // заголовок архивов
-  					if (is_day()) : printf('Daily Archives: %s', get_the_date()); // если по дням
-  					elseif (is_month()) : printf('Monthly Archives: %s', get_the_date('F Y')); // если по месяцам
-  					elseif (is_year()) : printf('Yearly Archives: %s', get_the_date('Y')); // если по годам
-  					else : 'Archives';
-  				endif; ?></h1>
-  				<?php if (have_posts()) : while (have_posts()) : the_post(); // если посты есть - запускаем цикл wp ?>
-  					<?php get_template_part('loop'); // для отображения каждой записи берем шаблон loop.php ?>
-  				<?php endwhile; // конец цикла
-  				else: echo '<p>Нет записей.</p>'; endif; // если записей нет, напишим "простите" ?>
-  				<?php pagination(); // пагинация, функция нах-ся в function.php ?>
-  			</div>
-  			<?php get_sidebar(); // подключаем sidebar.php ?>
-      -->
+
   		</div>
   	</div>
   </section>
