d209b4b119907cd741a79974336aa094c08c5793
shilovperm <46396241+shilovperm@users.noreply.github.com>
2019-01-15 00:04:30 +0500

    проблема отображения intro на мобильном телефоне

--- page-templates/intro.php
+++ page-templates/intro.php
@@ -4,37 +4,21 @@
 */
 
 get_header(); ?>
-    <!--
-    <div id="intro" class="view" style="background-image: url(<?php echo get_template_directory_uri() . '/images/title2.jpg'; ?>); -webkit-background-size:cover; background-size:cover;">
-        <div class="mask rgba-black-strong">
-            <div class="container-fluid d-flex align-items-center justify-content-center h-100">
-                <div class="row d-flex justify-content-center text-center">
-                    <h2 class="display-4 font-weight-bold white-text pt-5 mb-2">
-                       Любишь гоняться? Люби и гоняйся
-                    </h2>
-                    <hr class="hr-light">
-                    <h4 class="white-text">
-                      Мы тебе гонки - ты нам свою душу
-                    </h4>
-                    <button type="button" class="btn btn-outline-light">Начать!</button>
-                </div>
-            </div>
-        </div>
-    </div>
-  -->
+
     <div class="container-intro">
         <div class="view">
             <img src="<?php echo get_template_directory_uri() . '/images/title2.jpg'; ?>" class="img-fluid" alt="">
             <div class="mask flex-center rgba-black-strong">
-                <div class="row d-flex flex-column justify-content-center text-center h-100">
-                    <h3 class="text-light pt-5 mb-2">
+                <div class="row d-flex flex-column justify-content-end text-center h-100">
+                    <h3 class="text-light pt-5 mb-5">
                        Любишь гоняться? Люби и гоняйся
                     </h3>
-                    <hr class="hr-light">
-                    <h4 class="text-light">
+                    <!--<hr class="hr-light">
+                      <h4 class="text-light">
                       Мы тебе гонки - ты нам свою душу
-                    </h4>
-                    <a class="btn btn-outline-light align-self-center" href="<?php echo home_url() . '/events/';?>">Начать!</a>
+                    </h4>-->
+                    <a class="btn btn-outline-light align-self-center mb-5" href="<?php echo home_url() . '/events/';?>">Начать!</a>
+
                 </div>
             </div>
         </div>
