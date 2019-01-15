7bbfe036adef9600ae3b5770e184dd59ed13bff0
Vladimir Shilov <46396241+shilovperm@users.noreply.github.com>
2019-01-15 10:26:24 +0500

    Исправление мобильной версии

--- page-templates/intro.php
+++ page-templates/intro.php
@@ -9,15 +9,15 @@ get_header(); ?>
         <div class="view">
             <img src="<?php echo get_template_directory_uri() . '/images/title2.jpg'; ?>" class="img-fluid" alt="">
             <div class="mask flex-center rgba-black-strong">
-                <div class="row d-flex flex-column justify-content-end text-center h-100">
-                    <h3 class="text-light pt-5 mb-5">
+                <div class="row d-flex flex-column justify-content-center text-center h-100 container-fluid">
+                    <h3 class="text-light pt-2 mb-2 container-fluid">
                        Любишь гоняться? Люби и гоняйся
                     </h3>
                     <!--<hr class="hr-light">
                       <h4 class="text-light">
                       Мы тебе гонки - ты нам свою душу
                     </h4>-->
-                    <a class="btn btn-outline-light align-self-center mb-5" href="<?php echo home_url() . '/events/';?>">Начать!</a>
+                    <a class="btn btn-outline-light align-self-center mb-2" href="<?php echo home_url() . '/events/';?>">Начать!</a>
 
                 </div>
             </div>
