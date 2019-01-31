87d8dac8d6f146c5cb2ca44c1ce4f5e8b8881b11
Vladimir Shilov <46396241+shilovperm@users.noreply.github.com>
2019-01-31 17:43:29 +0500

    google analitycs

--- header.php
+++ header.php
@@ -13,6 +13,15 @@
 <!doctype html>
 <html <?php language_attributes(); ?>>
 <head>
+	<!-- Global site tag (gtag.js) - Google Analytics -->
+	<script async src="https://www.googletagmanager.com/gtag/js?id=UA-133587744-1"></script>
+	<script>
+	  window.dataLayer = window.dataLayer || [];
+	  function gtag(){dataLayer.push(arguments);}
+	  gtag('js', new Date());
+	  gtag('config', 'UA-133587744-1');
+	</script>
+
 	<meta charset="<?php bloginfo( 'charset' ); ?>">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<link rel="profile" href="http://gmpg.org/xfn/11">
