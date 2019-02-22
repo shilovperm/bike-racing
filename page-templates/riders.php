784a612895d0b1ba102cec57277d0d77e1d01b02
Vladimir Shilov <46396241+shilovperm@users.noreply.github.com>
2019-02-22 11:40:54 +0500

    error img

--- page-templates/riders.php
+++ page-templates/riders.php
@@ -24,9 +24,9 @@ get_header(); ?>
               }
               echo '  </td>';
               echo '  <td>';
-              if (strlen($rider_value->team_name)>0){
-                echo '<a href="'.$rider_value->team_strava_link.'" target="_blank"><img  class="ml-1" src="data:image/png;base64,'.base64_encode($rider_value->team_image).'" height="24" alt="'.$rider_value->team_name.'"></a>';
-              }
+              /*if (strlen($rider_value->team_name)>0){
+                echo '<a href="'.$rider_value->team_strava_link.'" target="_blank"><img  class="ml-1" src="data:image/png;base64,'.base64_encode($rider_value->team_image).'" max-height="24" alt="'.$rider_value->team_name.'"></a>';
+              }*/
 
               echo '  </td>';
               echo '</tr>';
