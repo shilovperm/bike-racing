b5fce53dbf8393268581cc24bdd536889dd3d5ae
shilovperm <46396241+shilovperm@users.noreply.github.com>
2019-02-24 23:26:54 +0500

    tooltips

--- assets/js/customizer.js
+++ assets/js/customizer.js
@@ -26,6 +26,6 @@ $(document).ready(function () {
 
  });
 
- $(function () {
-   $('[data-toggle="tooltip"]').tooltip()
- });
+ $(document).ready(function () {
+             $('[data-toggle="tooltip"]').tooltip();
+         });
--- functions.php
+++ functions.php
@@ -148,6 +148,7 @@ function wp_bootstrap_4_scripts() {
 	wp_enqueue_style( 'bootstrap-4', get_template_directory_uri() . '/assets/css/bootstrap.css', array(), 'v4.0.0', 'all' );
 	wp_enqueue_style( 'wp-bootstrap-4-style', get_stylesheet_uri(), array(), '1.0.2', 'all' );
 
+	wp_enqueue_script( 'popper-js', get_template_directory_uri() . '/assets/js/popper.js', array('jquery'), 'v', true );
 	wp_enqueue_script( 'bootstrap-4-js', get_template_directory_uri() . '/assets/js/bootstrap.js', array('jquery'), 'v4.0.0', true );
   wp_enqueue_script( 'customizer-js', get_template_directory_uri() . '/assets/js/customizer.js', array('jquery'), 'v1.0', true );
 
