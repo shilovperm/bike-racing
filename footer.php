a576012760dca4058b86697abcc4c3d61b7c8764
Vladimir Shilov <46396241+shilovperm@users.noreply.github.com>
2019-04-04 16:48:10 +0500

    add new footer

--- footer.php
+++ footer.php
@@ -17,11 +17,18 @@
 			<div class="container">
 				<div class="row">
 						<div class="col-sm">
+							<h6 style="color:white!important;">Полезные ссылки</h6>
+							<a href="https://t.me/cyclingche" 	target="_blank" style="color:white!important;"><img class="icon-social" src="<?php echo get_template_directory_uri()?>/images/telegram_logo.png">МТБ Велошкола Че</a><br>
+							<a href="https://vk.com/cyclingche" target="_blank" style="color:white!important;"><img class="icon-social" src="<?php echo get_template_directory_uri()?>/images/vk-logo.png">МТБ Велошкола Че</a>
+						</div>
+						<div class="col-sm">
+							<h6 style="color:white!important;">Обратная связь</h6>
+							<a href="https://t.me/bike_racing_ru" target="_blank"><img class="d-inline icon-social" src="<?php echo get_template_directory_uri()?>/images/telegram_logo.png"> </a>
+							<a href="https://vk.com/bike_racing_ru" target="_blank"><img class="d-inline icon-social" src="<?php echo get_template_directory_uri()?>/images/vk-logo.png"></a>
+						</div>
+						<div class="col-sm">
+							<h6 style="color:white!important;">Прочее</h6>
 							<a href="<?php echo home_url() . '/policy';?>" style="color:white!important;"> Политика конфиденциальности </a>
-						</div>
-						<div class="col-sm">
-						</div>
-						<div class="col-sm">
 						</div>
 				</div>
 			</div>
