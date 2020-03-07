8d728af37a25749936d5b8910f684f8a5a3861c4
Vladimir Shilov <46396241+shilovperm@users.noreply.github.com>
2020-03-07 18:54:43 +0500

    calendar v.3

--- functions.php
+++ functions.php
@@ -654,6 +654,25 @@ function set_wp_user_verified($rider_id)
 		return $results;
 	}
 
+	//Преобразование даты в русское наименование
+	function russian_date($event_date){
+		$date=explode(".", date("j.m.Y", strtotime($event_date)));
+		switch ($date[1]){
+			case 1: $m='января'; break;
+			case 2: $m='февраля'; break;
+			case 3: $m='марта'; break;
+			case 4: $m='апреля'; break;
+			case 5: $m='мая'; break;
+			case 6: $m='июня'; break;
+			case 7: $m='июля'; break;
+			case 8: $m='августа'; break;
+			case 9: $m='сентября'; break;
+			case 10: $m='октября'; break;
+			case 11: $m='ноября'; break;
+			case 12: $m='декабря'; break;
+		}
+		echo $date[0].'&nbsp;'.$m.'&nbsp;';
+	}
 
 /*Заменяем логотип на свой*/
 function my_login_logo(){
--- page-templates/calendar.php
+++ page-templates/calendar.php
@@ -23,24 +23,36 @@ get_header(); ?>
         <?php foreach ($years as &$value) {
           $months = get_months_of_events_by_year($value->year);
         ?>
-          <div class="tab-pane fade <?php if ($value->year == $years[0]->year) { echo 'active show';} ?>" id="YEAR<?php echo $value->year?>" role="tabpanel" aria-labelledby="<?php echo $value->year?>-tab">
+          <div class="calendar-events tab-pane fade <?php if ($value->year == $years[0]->year) { echo 'active show';} ?>" id="YEAR<?php echo $value->year?>" role="tabpanel" aria-labelledby="<?php echo $value->year?>-tab">
+            <div class = "calendar-events">
             <?php foreach ($months as &$month_value) { ?>
-                <div class="card card-cascade card-month  position-relative blue-grey-text">
+                <div class="card card-cascade card-month position-relative blue-grey-text">
                   <p class="mb-0 text-center"><b><?php echo $month_name[$month_value->month_num-1] ?></b></p>
                   <?php $events = get_events_by_year_month($value->year, $month_value->month_num);
                     foreach ($events as &$event_value) { ?>
-                    <div class="card card-calendar text-white bg-secondary m-1 ml-3 mr-3" style="max-width: 18rem;">
-                      <div class="card-header p-0 d-flex justify-content-center"><?php echo $event_value->event_title?></div>
+                    <a href="event/?event_id=<?php  echo $event_value->event_id ?>" class="card card-calendar text-white
+                    <?php if ($event_value->status_id == 1) {echo 'bg-primary';}
+                          elseif ($event_value->status_id == 2) {echo 'bg-success';}
+                          elseif ($event_value->status_id == 3) {echo 'bg-secondary';}
+                          elseif ($event_value->status_id == 4) {echo 'bg-muted';}
+                          elseif ($event_value->status_id == 5) {echo 'bg-danger';}
+                    ?>
+                    m-1 ml-3 mr-3" style="max-width: 18rem;">
+                      <div class="card-header p-0 d-block justify-content-center">
+                        <p class="mb-0 text-center"><?php echo $event_value->event_title?></p>
+                        <p class="mb-0 text-center"><?php echo $event_value->event_subtitle?></p>
+                      </div>
                       <div class="card-body p-0">
                         <div class="d-flex justify-content-between p-0 pl-1 pr-2">
-                          <p class="card-text m-0"><?php echo $event_value->event_date?></p>
-                          <p class="card-text m-0"><?php echo $event_value->race_type_short_name?></p>
+                          <p class="card-text m-0"><?php echo russian_date($event_value->event_date) ?></p>
+                          <p class="card-text m-0"><?php echo $event_value->race_type_short_name ?></p>
                         </div>
                       </div>
-                    </div>
+                    </a>
                   <?php } ?>
                 </div>
             <?php } ?>
+            </div>
           </div>
         <?php } ?>
 
